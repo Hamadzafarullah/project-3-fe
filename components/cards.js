8e20e23036a0859e03dd7d183e23465332d9deb5
jay123son <youremail@domain.com>
2022-12-08 12:06:33 -0500

    adding cards

---
+++ components/cards.js
@@ -0,0 +1,137 @@
+import restaurantList from "../restaurantInfo"
+
+
+function cards(){
+  return(
+    <div className="cardbox">
+      <div className="cards">
+      <h2>{`${restaurantList[0].image}`}</h2>
+      <p>{`${restaurantList[0].location}`}</p>
+        <p>{`${restaurantList[0].cost}`}</p>
+        <p>{`${restaurantList[0].attreactions}`}</p>
+        <p>{`${restaurantList[0].information}`}</p>
+         
+            </div>
+
+
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[1].image}`}</h2>
+      <p>{`${restaurantList[1].location}`}</p>
+        <p>{`${restaurantList[1].cost}`}</p>
+        <p>{`${restaurantList[1].attreactions}`}</p>
+        <p>{`${restaurantList[1].information}`}</p>
+         
+         
+            </div>
+
+
+
+
+
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[2].image}`}</h2>
+      <p>{`${restaurantList[2].location}`}</p>
+        <p>{`${restaurantList[2].cost}`}</p>
+        <p>{`${restaurantList[2].attreactions}`}</p>
+        <p>{`${restaurantList[2].information}`}</p>
+         
+         
+            </div>
+
+
+
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[3].image}`}</h2>
+      <p>{`${restaurantList[3].location}`}</p>
+        <p>{`${restaurantList[3].cost}`}</p>
+        <p>{`${restaurantList[3].attreactions}`}</p>
+        <p>{`${restaurantList[3].information}`}</p>
+         
+         
+            </div>
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[4].image}`}</h2>
+      <p>{`${restaurantList[4].location}`}</p>
+        <p>{`${restaurantList[4].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[5].image}`}</h2>
+      <p>{`${restaurantList[5].location}`}</p>
+        <p>{`${restaurantList[5].cost}`}</p>
+         
+            </div>
+
+
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[6].image}`}</h2>
+      <p>{`${restaurantList[6].location}`}</p>
+        <p>{`${restaurantList[6].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[7].image}`}</h2>
+      <p>{`${restaurantList[7].location}`}</p>
+        <p>{`${restaurantList[7].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[8].image}`}</h2>
+      <p>{`${restaurantList[8].location}`}</p>
+        <p>{`${restaurantList[8].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[9].image}`}</h2>
+      <p>{`${restaurantList[9].location}`}</p>
+        <p>{`${restaurantList[9].cost}`}</p>
+         
+            </div>
+
+
+
+            <div className="cards">
+      <h2>{`${restaurantList[10].image}`}</h2>
+      <p>{`${restaurantList[10].location}`}</p>
+        <p>{`${restaurantList[10].cost}`}</p>
+         
+            </div>
+            
+            <div className="cards">
+      <h2>{`${restaurantList[11].image}`}</h2>
+      <p>{`${restaurantList[11].location}`}</p>
+        <p>{`${restaurantList[11].cost}`}</p>
+         
+            </div>
+            
+
+            </div>
+            
+            
+  )}
+ 
+
+
+  export default cards;
