04fafccda04a1c030fcf8fba66b0f44951fa84f2
sky0815a <sky0815a@gmail.com>
2022-12-08 14:01:58 -0500

    Added: home, navbar, title

---
+++ components/title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+
+const title = () => {
+  return (
+    <div>
+      <h2>search your dream vacation!</h2>
+      <Navbar />
+    </div>
+  );
+}
+export default title
